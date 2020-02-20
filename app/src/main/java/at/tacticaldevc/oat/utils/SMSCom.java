f784d7e142ab23ab08e742a48d5f902b80536e08
FiryDragon <47172991+FiryDragon1@users.noreply.github.com>
2020-02-20 17:17:08 +0100

    Added ability to request current position via SMS

--- app/src/main/java/at/tacticaldevc/oat/utils/SMSCom.java
+++ app/src/main/java/at/tacticaldevc/oat/utils/SMSCom.java
@@ -1,7 +1,80 @@
 package at.tacticaldevc.oat.utils;
 
+import android.content.Context;
+import android.location.Location;
+import android.telephony.SmsManager;
+import android.telephony.SubscriptionManager;
+
+import at.tacticaldevc.oat.R;
+
+import static at.tacticaldevc.oat.utils.Ensurer.ensureNotNull;
+import static at.tacticaldevc.oat.utils.Ensurer.ensurePhoneNumberIsValid;
+import static at.tacticaldevc.oat.utils.Ensurer.ensureStringIsValid;
+
 /**
  * A helper class for SMS communication
+ *
+ * @version 0.1
  */
 public class SMSCom {
+
+    /**
+     * Send a reply to let the User know, that a certain feature was not enabled
+     *
+     * @param context     the {@link Context} of the Application
+     * @param phoneNumber the phone number that requested the feature to be activated
+     * @param featureName the feature that could not be used, as it was deactivated
+     */
+    public static void replyErrorSMS_FeatureDisabled(Context context, String phoneNumber, String featureName) {
+        ensureNotNull(context, "Application Context");
+        ensurePhoneNumberIsValid(phoneNumber, "target phone number");
+        ensureStringIsValid(featureName, "name of disabled feature");
+
+        SmsManager smsManager = fetchSMSManager();
+        smsManager.sendTextMessage(phoneNumber, null, String.format(context.getString(R.string.oat_sms_message_error_feature_disabled), featureName), null, null);
+    }
+
+    /**
+     * Send a reply to let the User know, that the requested feature does not exist
+     *
+     * @param context     the {@link Context} of the Application
+     * @param phoneNumber the phone number that requested the non-existent feature
+     * @param featureName the name of the missing feature
+     */
+    public static void replyErrorSMS_FeatureNotFound(Context context, String phoneNumber, String featureName) {
+        ensureNotNull(context, "Application Context");
+        ensurePhoneNumberIsValid(phoneNumber, "phone number");
+        ensureStringIsValid(featureName, "feature name");
+
+        SmsManager smsManager = fetchSMSManager();
+        smsManager.sendTextMessage(phoneNumber, null, String.format(context.getString(R.string.oat_sms_message_error_feature_not_found), featureName), null, null);
+    }
+
+    /**
+     * Reply with the given GPS Position of the Device.
+     *
+     * @param context     the {@link Context} of the Application
+     * @param phoneNumber the phone number that asked for the GPS Position of the Device
+     * @param location    the current location of the Device
+     */
+    public static void replyFetchGPSPosition(Context context, String phoneNumber, Location location) {
+        ensureNotNull(context, "Application Context");
+        ensurePhoneNumberIsValid(phoneNumber, "target phone number");
+        ensureNotNull(location, "current location");
+
+        SmsManager smsManager = fetchSMSManager();
+        smsManager.sendTextMessage(phoneNumber, null, String.format(context.getString(R.string.oat_sms_message_send_gps_position), location.getLatitude(), location.getLongitude()), null, null);
+    }
+
+    /**
+     * @return the SMSManager associated with the default Subscription or null, if no valid subscription could be found
+     */
+    private static SmsManager fetchSMSManager() {
+        int subscriptionID = SmsManager.getDefaultSmsSubscriptionId();
+        if (!(subscriptionID == SubscriptionManager.INVALID_SUBSCRIPTION_ID)) {
+            return SmsManager.getSmsManagerForSubscriptionId(subscriptionID);
+        }
+        // Error handling missing
+        return null;
+    }
 }
--- app/src/main/java/at/tacticaldevc/oat/utils/Tracking.java
+++ app/src/main/java/at/tacticaldevc/oat/utils/Tracking.java
@@ -1,7 +1,52 @@
 package at.tacticaldevc.oat.utils;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.LocationManager;
+
+import at.tacticaldevc.oat.R;
+import at.tacticaldevc.oat.listeners.GPSListenerSMS;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+
 /**
  * A helper class for location tracking
+ *
+ * @version 0.1
  */
 public class Tracking {
+
+    /**
+     * Sends the current position via SMS
+     *
+     * @param context         the context of the Application
+     * @param phoneNumber     the phone number that should receive the reply
+     * @param locationManager (optional) the Location Manager to be used. If no Location Manager is specified, a new one is fetched.
+     * @throws SecurityException if the User revoked the Permission to access GPS without deactivating the feature
+     */
+    public static void sendCurrentCoordinatesViaSMS(Context context, String phoneNumber, LocationManager locationManager) throws SecurityException {
+        if (Prefs.fetchFeatureEnabledStatus(context, context.getString(R.string.oat_features_key_fetch_gps_position))) {
+            LocationManager lManager = locationManager;
+            if (locationManager == null)
+                locationManager = getSystemService(context, LocationManager.class);
+
+            // Location Listener
+            GPSListenerSMS gpsListener = new GPSListenerSMS(context, phoneNumber);
+
+            // Criteria
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+            criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+            criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+            criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+
+            locationManager.requestSingleUpdate(criteria, gpsListener, null);
+        } else {
+            SMSCom.replyErrorSMS_FeatureDisabled(context, phoneNumber, context.getString(R.string.oat_features_name_fetch_gps_position));
+        }
+    }
 }
