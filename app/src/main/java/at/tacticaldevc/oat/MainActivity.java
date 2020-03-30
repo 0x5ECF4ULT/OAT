014912527b142df5394862d2ce41ca32069c8544
FiryDragon1 <47172991+FiryDragon1@users.noreply.github.com>
2020-03-30 10:17:32 +0200

    Added required settings for testing

--- app/src/main/java/at/tacticaldevc/oat/MainActivity.java
+++ app/src/main/java/at/tacticaldevc/oat/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import at.tacticaldevc.oat.utils.Prefs;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        //testing
+        Prefs.savePassword(getApplicationContext(), "password", "password");
+        Prefs.saveTrustedContact(getApplicationContext(), "+15555215554", "Emulator Android 10");
+        Prefs.saveTrustedContact(getApplicationContext(), "+15555215556", "Emulator Android 7");
+        Prefs.saveFeatureEnabledStatus(getApplicationContext(), getApplicationContext().getString(R.string.oat_features_key_fetch_gps_position), true);
+        Prefs.saveFeatureEnabledStatus(getApplicationContext(), getApplicationContext().getString(R.string.oat_features_key_lift_lockdown), true);
+        Prefs.saveFeatureEnabledStatus(getApplicationContext(), getApplicationContext().getString(R.string.oat_features_key_trigger_lockdown), true);
+        Prefs.saveFeatureEnabledStatus(getApplicationContext(), getApplicationContext().getString(R.string.oat_features_key_trigger_photo_trap), true);
     }
 
 }
