b4ec54be7a612543494d536dd743b5dbe59ba375
FiryDragon1 <47172991+FiryDragon1@users.noreply.github.com>
2020-05-04 10:07:28 +0200

    Added Tests for OATApplicationException changes Added missing Tests for OATApplicationException

--- app/src/test/java/at/tacticaldevc/oat/utils/OATApplicationExceptionTest.java
+++ app/src/test/java/at/tacticaldevc/oat/utils/OATApplicationExceptionTest.java
@@ -3,6 +3,7 @@ package at.tacticaldevc.oat.utils;
 import org.junit.jupiter.api.Test;
 
 import at.tacticaldevc.oat.exceptions.OATApplicationException;
+import at.tacticaldevc.oat.exceptions.OATApplicationExceptionType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,11 +11,12 @@ public class OATApplicationExceptionTest {
     @Test
     void forLibraryError() {
         // test
-        OATApplicationException ex = OATApplicationException.forLibraryDeprecatedError("test library", new Exception("test Exception"));
+        OATApplicationException ex = OATApplicationException.forLibraryError("test library", new Exception("test Exception"));
         // assert
         assertThat(ex).isInstanceOf(OATApplicationException.class);
         assertThat(ex.getMessage()).contains("test library");
         assertThat(ex.getCause()).hasMessage("test Exception");
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.LibraryError);
     }
 
     @Test
@@ -24,6 +26,7 @@ public class OATApplicationExceptionTest {
         // assert
         assertThat(ex).isInstanceOf(OATApplicationException.class);
         assertThat(ex).hasMessage("No password has been set!");
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.NoPasswordSet);
     }
 
     @Test
@@ -33,6 +36,7 @@ public class OATApplicationExceptionTest {
         // assert
         assertThat(ex).isInstanceOf(OATApplicationException.class);
         assertThat(ex).hasMessage("No Subscription could be obtained to send a SMS!");
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.NoSMSSubscriptionFound);
     }
 
     @Test
@@ -42,6 +46,7 @@ public class OATApplicationExceptionTest {
         // assert
         assertThat(ex).isInstanceOf(OATApplicationException.class);
         assertThat(ex).hasMessage("The supplied old password did not match the saved password. The password was not updated.");
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.PasswordMismatch);
     }
 
     @Test
@@ -51,5 +56,44 @@ public class OATApplicationExceptionTest {
         // assert
         assertThat(ex).isInstanceOf(OATApplicationException.class);
         assertThat(ex).hasMessage("The password hash was corrupted. The password was deleted and a new password has to be set in the Application Settings.");
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.CorruptedPasswordHash);
+    }
+
+    @Test
+    void forLibraryDeprecated() {
+        // prepare
+        String testLibrary = "Test library";
+        Exception testException = new Exception(("Test exception"));
+        // test
+        OATApplicationException ex = OATApplicationException.forLibraryDeprecatedError(testLibrary, testException);
+        // assert
+        assertThat(ex).isInstanceOf(OATApplicationException.class);
+        assertThat(ex).hasMessage("The following library seems to no longer support a required feature: " + testLibrary);
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.LibraryDeprecated);
+    }
+
+    @Test
+    void forFeatureNotSupported() {
+        // prepare
+        String testFeature = "Test feature";
+        // test
+        OATApplicationException ex = OATApplicationException.forFeatureNotSupported(testFeature);
+        // assert
+        assertThat(ex).isInstanceOf(OATApplicationException.class);
+        assertThat(ex).hasMessage("The following feature can not be executed because of unsatisfied requirements: " + testFeature);
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.FeatureNotSupported);
+    }
+
+    @Test
+    void forSecurityException() {
+        // prepare
+        String testLibrary = "Test library";
+        Exception testException = new Exception(("Test exception"));
+        // test
+        OATApplicationException ex = OATApplicationException.forSecurityException(testLibrary, testException);
+        // assert
+        assertThat(ex).isInstanceOf(OATApplicationException.class);
+        assertThat(ex).hasMessage("The following library has thrown a SecurityException: " + testLibrary);
+        assertThat(ex.getExceptionType()).isEqualTo(OATApplicationExceptionType.SecurityException);
     }
 }
