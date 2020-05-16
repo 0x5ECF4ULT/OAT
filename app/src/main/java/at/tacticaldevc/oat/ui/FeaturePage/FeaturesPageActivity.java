081adb01a424699cab0804f2b5e4f23ee3ce5574
Jakob Guggenberger <bokay6000@gmail.com>
2020-05-16 18:17:46 +0200

    Implemented Photo Trap Settings Page, Instant Photo Setting Page and GPS Tracking Settings Page Restored missing Password Settings Page

--- app/src/main/java/at/tacticaldevc/oat/ui/FeaturePage/FeaturesPageActivity.java
+++ app/src/main/java/at/tacticaldevc/oat/ui/FeaturePage/FeaturesPageActivity.java
@@ -1,16 +1,49 @@
 package at.tacticaldevc.oat.ui.FeaturePage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import at.tacticaldevc.oat.R;
+import at.tacticaldevc.oat.ui.FeaturePage.InstantPhoto.InstantPhotoActivity;
+import at.tacticaldevc.oat.ui.FeaturePage.PhotoTrap.PhotoTrapActivity;
+import at.tacticaldevc.oat.ui.FeaturePage.Tracking.TrackingActivity;
 
 public class FeaturesPageActivity extends AppCompatActivity {
+
+    private Button photoTrap;
+    private Button instantPhoto;
+    private Button tracking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_features_page);
+
+        photoTrap = findViewById(R.id.feature_settings_phototrap);
+        photoTrap.setOnClickListener(cl -> openActivityPhotoTrap());
+
+        instantPhoto = findViewById(R.id.feature_settings_instant_photo);
+        instantPhoto.setOnClickListener(cl -> openActivityInstantPhoto());
+
+        tracking = findViewById(R.id.feature_settings_tracking);
+        tracking.setOnClickListener(cl -> openActivityTracking());
+    }
+
+    public void openActivityPhotoTrap(){
+        Intent intent = new Intent(this, PhotoTrapActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityInstantPhoto(){
+        Intent intent = new Intent(this, InstantPhotoActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityTracking(){
+        Intent intent = new Intent(this, TrackingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/at/tacticaldevc/oat/ui/FeaturePage/InstantPhoto/InstantPhotoActivity.java
@@ -0,0 +1,58 @@
+package at.tacticaldevc.oat.ui.FeaturePage.InstantPhoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Switch;
+
+import at.tacticaldevc.oat.R;
+import at.tacticaldevc.oat.utils.Prefs;
+
+public class InstantPhotoActivity extends AppCompatActivity {
+
+    private CheckBox accept;
+    private Switch activate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instant_photo);
+
+        accept = findViewById(R.id.instant_photo_checkbox);
+        accept.setOnClickListener(cl -> conditionsAcceptedCheck());
+
+        activate = findViewById(R.id.instant_photo_switch);
+        activate.setOnClickListener(cl -> activateFeature());
+        activate.setClickable(false);
+
+        featureActiveCheck();
+    }
+
+    public void featureActiveCheck(){
+        if(Prefs.fetchFeatureEnabledStatus(this,getString(R.string.oat_features_key_trigger_photo_trap))) {
+            accept.setChecked(true);
+            activate.setChecked(true);
+        }else{
+            accept.setChecked(false);
+            activate.setChecked(false);
+        }
+    }
+
+    public void conditionsAcceptedCheck(){
+        if(!accept.isChecked()){
+            activate.setChecked(false);
+            activate.setClickable(false);
+        }else{
+            activate.setClickable(true);
+        }
+    }
+
+    public void activateFeature(){
+        if(activate.isChecked()){
+            //Prefs.saveFeatureEnabledStatus(this, getString(R.string.oat_features_key_fetch_gps_position), true);
+        }else{
+            //Prefs.saveFeatureEnabledStatus(this, getString(R.string.oat_features_key_fetch_gps_position), false);
+        }
+    }
+}
