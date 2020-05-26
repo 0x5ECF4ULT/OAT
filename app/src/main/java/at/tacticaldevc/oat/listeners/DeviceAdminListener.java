7f173073d9d4f22aa6b5ee04195b8792171bdf9d
tacticalDevC <tacticaldevc@tutanota.com>
2020-05-26 10:50:30 +0200

    Fully implemented device admin

---
+++ app/src/main/java/at/tacticaldevc/oat/listeners/DeviceAdminListener.java
@@ -0,0 +1,40 @@
+package at.tacticaldevc.oat.listeners;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import at.tacticaldevc.oat.R;
+import at.tacticaldevc.oat.utils.Prefs;
+
+public class DeviceAdminListener extends DeviceAdminReceiver {
+
+    private static final DeviceAdminListener dal;
+
+    static {
+        dal = new DeviceAdminListener();
+    }
+
+    private DeviceAdminListener() {
+    }
+
+    public static DeviceAdminListener getDAObject() {
+        return dal;
+    }
+
+    @Override
+    public void onEnabled(@NonNull Context context, @NonNull Intent intent) {
+        super.onEnabled(context, intent);
+        Prefs.saveFeatureEnabledStatus(context, context.getString(R.string.oat_features_key_trigger_lockdown), true);
+        Prefs.saveFeatureEnabledStatus(context, context.getString(R.string.oat_features_key_lift_lockdown), true);
+    }
+
+    @Override
+    public void onDisabled(@NonNull Context context, @NonNull Intent intent) {
+        super.onDisabled(context, intent);
+        Prefs.saveFeatureEnabledStatus(context, context.getString(R.string.oat_features_key_trigger_lockdown), false);
+        Prefs.saveFeatureEnabledStatus(context, context.getString(R.string.oat_features_key_lift_lockdown), false);
+    }
+}
--- app/src/main/java/at/tacticaldevc/oat/utils/DA.java
+++ app/src/main/java/at/tacticaldevc/oat/utils/DA.java
@@ -1,7 +1,48 @@
 package at.tacticaldevc.oat.utils;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.Context;
+
+import at.tacticaldevc.oat.R;
+import at.tacticaldevc.oat.listeners.DeviceAdminListener;
+
+import static at.tacticaldevc.oat.utils.Ensurer.ensureNotNull;
+import static at.tacticaldevc.oat.utils.Ensurer.ensureStringIsValid;
+
 /**
  * A helper class for device administration
  */
 public class DA {
+
+    /**
+     * Activate the lockdown using DA
+     *
+     * @param ctx   The application context
+     * @param phone A valid phone number in String representation
+     */
+    public static void lockdown_activate(Context ctx, String phone) {
+        ensureNotNull(ctx, "Context");
+        ensureStringIsValid(phone, "Phone number");
+
+        if (Prefs.fetchFeatureEnabledStatus(ctx, ctx.getString(R.string.oat_features_key_trigger_lockdown))) {
+            DevicePolicyManager pol = DeviceAdminListener.getDAObject().getManager(ctx);
+            pol.lockNow();
+        }
+    }
+
+    /**
+     * Deactivate the lockdown using DA
+     *
+     * @param ctx   The application context
+     * @param phone A valid phone number in String representation
+     */
+    public static void lockdown_deactivate(Context ctx, String phone) {
+        ensureNotNull(ctx, "Context");
+        ensureStringIsValid(phone, "Phone number");
+
+        if (Prefs.fetchFeatureEnabledStatus(ctx, ctx.getString(R.string.oat_features_key_trigger_lockdown))) {
+            DevicePolicyManager pol = DeviceAdminListener.getDAObject().getManager(ctx);
+            pol.lockNow();
+        }
+    }
 }
